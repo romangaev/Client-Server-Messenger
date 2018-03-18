f99f9bc60f6213bd74673b881a56ba541db80d4b
Rom <roman.a.gaev@yandex.ru>
2018-03-18 15:10:40 +0000

    doing list for interfce

--- src/NewServerThread.java
+++ src/NewServerThread.java
@@ -1,10 +1,11 @@
 import java.io.*;
+import java.net.ServerSocket;
 import java.net.Socket;
-import java.sql.*;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 /**
  * Initially created by Roman Gaev
@@ -16,19 +17,16 @@ import java.util.Map;
 public class NewServerThread extends Thread {
     private ServerModel server;
     public Socket client;
-    private Connection connection;
     private Statement statement;
     private User currentUser;
     private ObjectInputStream ois;
     private ObjectOutputStream oos;
-
     //constructor with database connection and client's socket
-    public NewServerThread(ServerModel server, Socket client, Connection connection) throws IOException, SQLException {
+    public NewServerThread(ServerModel server, Socket client, Statement statement) throws IOException {
         super("NewServerThread");
         this.server = server;
         this.client = client;
-        this.connection= connection;
-        this.statement = connection.createStatement();
+        this.statement = statement;
         oos = new ObjectOutputStream(client.getOutputStream());
         ois = new ObjectInputStream(client.getInputStream());
 
@@ -70,72 +68,37 @@ public class NewServerThread extends Thread {
         }
     }
 
-    //Sending message as an object and storing it in the database
     public void sendMessage(Message message) {
         try {
-            String from = message.getContent()[0];
-            int to = Integer.valueOf(message.getContent()[1]);
-            String content = message.getContent()[2];
-
-            //Sending message to everyone in corresponding group
-            executePreStatement();
-            ResultSet rs = statement.executeQuery("SELECT username FROM groups where id="+to);
-            while(rs.next()){
-                String userToSend=rs.getString(1);
-                List<NewServerThread> pool = server.getThreadPool();
-                pool.forEach(x -> {
-                    String otherUser = x.getCurrentUser().getLogin();
-                    //   if(otherUser!=null&&!otherUser.getLogin().equals(currentUser.getLogin())) x.out.println(Protocol.MESSAGE +" "+currentUser.getLogin()+" "+ msg);
-                    if (otherUser.equals(userToSend)) {
-                        try {
-                            x.getOut().writeObject(message);
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
+            List<NewServerThread> pool = server.getThreadPool();
+            pool.forEach(x -> {
+                User otherUser = x.getCurrentUser();
+                //   if(otherUser!=null&&!otherUser.getLogin().equals(currentUser.getLogin())) x.out.println(Protocol.MESSAGE +" "+currentUser.getLogin()+" "+ msg);
+                if (!x.equals(this) && otherUser != null) {
+                    try {
+                        x.getOut().writeObject(message);
+                    } catch (IOException e) {
+                        e.printStackTrace();
                     }
-                });
-            }
-
-            //Inserting to the database
-            executePreStatement();
-            ResultSet rs1 = statement.executeQuery("SELECT MAX(id) FROM users");
-            rs1.next();
-            int nextId = rs1.getInt(1) + 1;
-            PreparedStatement ps = connection.prepareStatement("INSERT INTO messages VALUES (?, ?, ?, ?, ?)");
-            ps.setInt(1,nextId);
-            ps.setInt(2,to);
-            ps.setString(3,from);
-            ps.setString(4,content);
-            ps.setTimestamp(5,new Timestamp(System.currentTimeMillis()));
-            File file = message.getFile();
-            if(file!=null){
-                ps.setBytes(6, ObjectConverter.getByteArrayObject(file));
-            }
-            ps.executeUpdate();
-            ps.close();
+                }
+            });
         }catch (Exception e){e.printStackTrace();}
     }
 
-
     public void register(String username, String password, String legalname) throws IOException {
         try {
-            //Creating private conversation group with every user in the database
-            executePreStatement();
-            ResultSet rs1 = statement.executeQuery("SELECT * FROM users");
-            while(rs1.next()) {
-                ResultSet rs = statement.executeQuery("SELECT MAX(id) FROM groups");
-                rs.next();
-                int nextGroupId = rs.getInt(1) + 1;
-                statement.executeUpdate("INSERT INTO groups VALUES ('" + nextGroupId + "','private','" + rs1.getString(2) + "')");
-                statement.executeUpdate("INSERT INTO groups VALUES ('" + nextGroupId + "','private','" + username + "')");
-            }
-
-            //Creating user in users table
+            statement.executeQuery("SELECT\n" +
+                    "    table_schema || '.' || table_name\n" +
+                    "FROM\n" +
+                    "    information_schema.tables\n" +
+                    "WHERE\n" +
+                    "    table_type = 'BASE TABLE'\n" +
+                    "AND\n" +
+                    "    table_schema NOT IN ('pg_catalog', 'information_schema');");
             ResultSet rs = statement.executeQuery("SELECT MAX(id) FROM users");
             rs.next();
             int nextId = rs.getInt(1) + 1;
             statement.executeUpdate("INSERT INTO users VALUES ('" + nextId + "','" + username + "','" + password + "','" + legalname + "')");
-
             oos.writeObject(new Message(Protocol.TRUE));
         } catch (SQLException e) {
             e.printStackTrace();
@@ -143,39 +106,24 @@ public class NewServerThread extends Thread {
         }
     }
 
-
-
     public void login(String username, String password) throws IOException {
         try {
-            //checking user existence and password
-            executePreStatement();
+            statement.executeQuery("SELECT\n" +
+                    "    table_schema || '.' || table_name\n" +
+                    "FROM\n" +
+                    "    information_schema.tables\n" +
+                    "WHERE\n" +
+                    "    table_type = 'BASE TABLE'\n" +
+                    "AND\n" +
+                    "    table_schema NOT IN ('pg_catalog', 'information_schema');");
+
             ResultSet rs = statement.executeQuery("SELECT username, password,name FROM users WHERE username = '" + username + "'");
                 if (rs.next()&&rs.getString(1).equals(username) && rs.getString(2).equals(password)) {
                     currentUser = new User(rs.getString(1), rs.getString(2), rs.getString(3));
                     oos.writeObject(new Message(Protocol.TRUE));
 
-                    // send to user conversations information
-                    //creating a MAP where we will store conversation info and send it to client to show as contact list
-                   Map<Integer,Conversation> conversations = new HashMap<>();
-                   ResultSet ids = statement.executeQuery("SELECT group_id FROM groups WHERE username='"+username+"'");
-                   while(ids.next()){
-                       Statement statement2 = connection.createStatement();
-                       ResultSet rs2 = statement2.executeQuery("SELECT * FROM groups WHERE group_id='"+ids.getInt(1)+"'");
-                       while(rs2.next()){
-                           int id=rs2.getInt(1);
-                           String user= rs2.getString(2);
-                           String groupName= rs2.getString(3);
-
-                           if(!conversations.containsKey(id))
-                               conversations.put(id, new Conversation(groupName,user));
-                           else conversations.get(id).getParticipants().add(user);
-                       }
-                   }
-
-                   oos.writeObject(new HashMap<Integer,Conversation>());
-
-                    // send current user all other online logins
                     ArrayList<NewServerThread> pool = server.getThreadPool();
+                    // send current user all other online logins
                     pool.forEach(x -> {
                                 User threadUser = x.getCurrentUser();
                                 if (threadUser != null && !threadUser.getLogin().equals(getCurrentUser().getLogin())) {
@@ -210,7 +158,6 @@ public class NewServerThread extends Thread {
 
     }
 
-
     private void logoff() throws IOException {
         System.out.println("logoff");
         server.getThreadPool().remove(this);
@@ -235,17 +182,6 @@ public class NewServerThread extends Thread {
 
     public User getCurrentUser() {
         return currentUser;
-    }
-
-    public void executePreStatement() throws SQLException {
-        statement.executeQuery("SELECT\n" +
-                "    table_schema || '.' || table_name\n" +
-                "FROM\n" +
-                "    information_schema.tables\n" +
-                "WHERE\n" +
-                "    table_type = 'BASE TABLE'\n" +
-                "AND\n" +
-                "    table_schema NOT IN ('pg_catalog', 'information_schema');");
     }
 
 
